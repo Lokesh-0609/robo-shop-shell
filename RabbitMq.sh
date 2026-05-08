017464153f31053abc6aad6916cb0abf434c3ae3
LokeshThokala123 <lokeshibmi@gmail.com.com>
2026-05-08 07:30:33 +0530

    catalogue

--- RabbitMq.sh
+++ RabbitMq.sh
@@ -1,20 +1,13 @@
-cat > /etc/yum.repos.d/rabbitmq_erlang.repo << 'EOF'
-[rabbitmq_erlang]
-name=rabbitmq_erlang
-baseurl=https://packagecloud.io/rabbitmq/erlang/el/9/$basearch
-gpgcheck=0
-enabled=1
-EOF
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq_erlang.repo
 dnf install -y erlang
-
-cat > /etc/yum.repos.d/rabbitmq_rabbitmq-server.repo << 'EOF'
-[rabbitmq_rabbitmq-server]
-name=rabbitmq_rabbitmq-server
-baseurl=https://packagecloud.io/rabbitmq/rabbitmq-server/el/9/$basearch
-gpgcheck=0
-enabled=1
-EOF
 dnf install -y rabbitmq-server
 
 systemctl enable rabbitmq-server
 systemctl restart rabbitmq-server
+
+rabbitmq-plugins enable rabbitmq_management
+systemctl restart rabbitmq-server
+
+rabbitmqctl add_user roboshop RoboShop@1
+rabbitmqctl set_user_tags roboshop administrator
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
