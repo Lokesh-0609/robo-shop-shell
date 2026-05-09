18df5cfc3baed40a81c75e6345ac5fa4b1417a17
LokeshThokala123 <lokeshibmi@gmail.com.com>
2026-05-09 09:06:40 +0530

    catalogue

---
+++ echo-test2-redire.sh
@@ -0,0 +1,25 @@
+# install nginx
+echo -e "\\e[33m >>>>> This is for cp nginx file <<<<<<<<< \\e[0m"
+cp nginx.conf /etc/nginx/nginx.conf &>>/tmp/roboshop.log
+dnf install -y nginx &>>/tmp/roboshop.log
+##Copy file from /etc/nginx/nginx.conf
+echo -e "\e[33m >>>>>>>>> this is for nodejs setup <<<<<<<<<<<< \e[0m"
+curl -fsSL https://rpm.nodesource.com/setup_20.x | bash -  &>>/tmp/roboshop.log
+echo -e '\e[33m >>>>>>>>>> this is for install nodejs setup <<<<<<<<<<<<<< \e[0m'
+dnf install -y nodejs &>>/tmp/roboshop.log
+
+echo -e "\e[33m >>>>>>>>>>> this is for install frontend setup <<<<<<<<<<<< \e[0m"
+
+curl -L -o /tmp/frontend.zip https://raw.githubusercontent.com/raghudevopsb89/roboshop-microservices/main/artifacts/frontend.zip &>>/tmp/roboshop.log
+rm -rf  /tmp/frontend
+mkdir -p /tmp/frontend && cd /tmp/frontend
+unzip /tmp/frontend.zip
+echo -e "\e[31m >>>>>>>>>>>>>>>>>> this is for install npm <<<<<<<<<<<<<<< \e[0m"
+npm install &>>/tmp/roboshop.log
+echo -e "\e[31m >>>>>>>>>>>> this is for install npm build <<<<<<<<<<<<<< \e[0m"
+npm run build &>>/tmp/roboshop.log
+rm -rf /usr/share/nginx/html/*
+cp -r out/* /usr/share/nginx/html/
+nginx -t
+systemctl enable nginx &>>/tmp/roboshop.log
+systemctl restart nginx &>>/tmp/roboshop.log
