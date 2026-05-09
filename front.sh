7f948cba92d7a0234bce6646c5cb374d7e038fef
LokeshThokala123 <lokeshibmi@gmail.com.com>
2026-05-09 07:54:54 +0530

    catalogue

--- front.sh
+++ front.sh
@@ -1,9 +1,15 @@
 # install nginx
+echo -e \\e[37mThis is for cp nginx file \\e[0m
+cp nginx.conf /etc/nginx/nginx.conf
 dnf install -y nginx
 ##Copy file from /etc/nginx/nginx.conf
-cp nginx.conf /etc/nginx/nginx.conf
+echo -e "\e[37m this is for nodejs setup \e[0m"
 curl -fsSL https://rpm.nodesource.com/setup_20.x | bash -
+echo -e '\e[37m this is for install nodejs setup \e[0m'
 dnf install -y nodejs
+
+echo -e "\e[37m this is for install frontend setup \e[0m"
+
 curl -L -o /tmp/frontend.zip https://raw.githubusercontent.com/raghudevopsb89/roboshop-microservices/main/artifacts/frontend.zip
 rm -rf  /tmp/frontend
 mkdir -p /tmp/frontend && cd /tmp/frontend
