2b1d213dc55811771bb8feb3f5cd48b67411f19e
LokeshThokala123 <lokeshibmi@gmail.com.com>
2026-05-09 08:33:41 +0530

    catalogue

--- echo-test.sh
+++ echo-test.sh
@@ -1,3 +1,5 @@
+
+echo hellow world
 echo -e \\e[37m >>>>>>>> Hello Lokesh <<<<<<<<<<<<<<< \\e [0m
 echo -e "\e[31m >>>>>>>>>>>> Hello Surendra <<<<<<<<<<<<<< \e [0m"
 echo -e '\e[37m >>>>> Hello Siva <<<<<<<<<<<<<< \e [0m'
