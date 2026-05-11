f47d26c3b77293e9a4528b280eadddb61ecf4857
LokeshThokala123 <lokeshibmi@gmail.com.com>
2026-05-11 13:42:23 +0530

    catalogue

--- 10-vars.sh
+++ 10-vars.sh
@@ -17,3 +17,17 @@ echo z from  input -${z}
 
 
 ## Out of four, right side using arguments are preferred by many engineers to write, As this is the standard way of most commands that execute in Linux.
+
+echo first argument is -$1
+echo seconde argument is $2
+###echo N th argument-${n} beyond $9, from $10, to get the value we should use ${10}
+echo all arguments- $*
+echo number of arguments- $#
+
+##command substitution
+date=$(date)
+echo date is -${date}
+
+##Arithemetic sub stitution
+sum=$((10+5))
+echo 'sum of 10+5 is -' ${sum}
