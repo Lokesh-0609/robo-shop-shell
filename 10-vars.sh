fbd40f4080884dbe0cc48dce9c9b592ac40a59a0
LokeshThokala123 <lokeshibmi@gmail.com.com>
2026-05-11 13:12:24 +0530

    catalogue

--- 10-vars.sh
+++ 10-vars.sh
@@ -7,3 +7,13 @@ echo  x from left side of  script - ${x}
 ## bash 10-vars.sh
 
 echo y from before script declared  - ${y}
+
+### bash 10-vars.sh call firts argument
+echo first arugument is - $1
+
+## after executing script
+read -p 'enter z is -' z
+echo z from  input -${z}
+
+
+## Out of four, right side using arguments are preferred by many engineers to write, As this is the standard way of most commands that execute in Linux.
