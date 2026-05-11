90b664e068cea9052a9c6cdcee5e66fb7d202a42
LokeshThokala123 <lokeshibmi@gmail.com.com>
2026-05-11 12:44:08 +0530

    catalogue

--- 09-vars.sh
+++ 09-vars.sh
@@ -1,12 +1,12 @@
 ### For loop
 for vegitables in bitterourd bottlegourd ladfinger cucumber; do
-  echo Vegitables name is - ${vedgitables}
+  echo Vegitables name is - ${vegitables}
   sleep 5
 done
 
 ### while loop
 x=10
 while [ $x -gt 0 ]; do
-  echo x is gt than ${x}
+  echo x is gt than- ${x}
   x=$(($x-1)) ## x=x-1
 done
