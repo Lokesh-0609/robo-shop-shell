707bf8730e7423f5fbb66e612dac443c4b697ac9
LokeshThokala123 <lokeshibmi@gmail.com.com>
2026-05-11 09:21:53 +0530

    catalogue

--- 08-if.sh
+++ 08-if.sh
@@ -1,13 +1,17 @@
 x=100
 
-if [ $x = 100 ]; then
+if [ $x = 100 ];then
    echo  x is 100
 fi
 
-if [ $x -eq 100 ]; then
+if [ $x -eq 100 ];then
    echo x is 100
 fi
 
 if [ $x -ge 200 ];then
    echo x is greater than 200
+fi
+
+if [ $x -le 200 ];then
+   echo x is less than 200
 fi
